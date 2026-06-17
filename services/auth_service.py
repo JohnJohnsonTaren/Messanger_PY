12b89e1fa9283090f407b0192c07b026c0105785
IbraUA <sergii.ezhov@gmail.com>
2026-06-17 17:34:24 +0300

    login, verification, pass check

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,6 +1,5 @@
-from utils.security import hash_password, verify_password
+from utils.security import hash_password, verify_password, validate_password
 from models.user import User
-
 
 class AuthService:
 
@@ -11,26 +10,27 @@ class AuthService:
         if not username.strip():
             print("Логін не може бути порожнім")
             return False
+
         existing_user = self.user_repository.find_by_username(username)
         if existing_user:
             print("Користувач з таким логіном вже існує")
             return False
-
+        if not validate_password(password):
+            return False
         password_hash = hash_password(password)
         user = User(id = None, username = username, password_hash = password_hash)
         self.user_repository.save(user)
         return True
 
-        # TODO:
-        # Проверка логина
-        # Проверка сложности пароля
-        # Хэширование пароля
-        # Сохранение пользователя
-        pass
-
     def login(self, username, password):
+        existing_user = self.user_repository.find_by_username(username)
+        if not existing_user:
+            return False
+        else:
+            if verify_password(password, existing_user.password_hash):
+                return True
+            else:
+                print(f"Логін або пароль невірні")
+                return False
 
-        # TODO:
-        # Проверка пользователя
-        pass
 
--- utils/security.py
+++ utils/security.py
@@ -1,3 +1,4 @@
+import re
 import bcrypt
 
 def hash_password(password):
@@ -5,3 +6,17 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return bcrypt.checkpw(password.encode('UTF-8'), hashed)
+
+def validate_password(password):
+    if len(password) >= 8 and re.search('[A-Z]', password) and re.search('[!@#$%^&*]', password) and re.search('[0-9]', password):
+        return True
+    else:
+        print(f"\nВаш пароль{password} не відповідає вимогам: ")
+        print("Мінімум 8 символів")
+        print("Хочаб 1 велика літера")
+        print("Хочаб один спецсимвол")
+        print("Хочаб одну цифру")
+
+        return False
+
+
